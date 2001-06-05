b558168f13a188a8743e7dd6fd64b8a4301460f5
raving <raving@7fb41c83-528f-4485-b1c9-26d8fb2e8106>
2001-06-05 20:42:24 +0000

    added seperator to options menu

--- Source/Menus.h
+++ Source/Menus.h
@@ -44,8 +44,8 @@ enum 	{EM_Undo = 1, EM_Sep1, EM_Cut, EM_Copy,
 		 
 enum	{SM_Find = 1, SM_FindForward, SM_FindBackward, SM_Sep1, SM_GotoAddress};
 
-enum	{OM_HiAscii = 1, OM_DecimalAddr, OM_VertBars, OM_WinSize, OM_Overwrite, OM_Sep1, OM_Backups,
-			OM_ComparePref};
+enum	{OM_HiAscii = 1, OM_DecimalAddr, OM_VertBars, OM_WinSize, OM_Overwrite, OM_Sep1,
+			 OM_Backups, OM_Sep2, OM_ComparePref};
 
 enum	{CM_UseColor = 1, CM_Sep1, CM_FirstColor};
 
